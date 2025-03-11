fad6fdde04afffdd04732072963412715e07f2ea
PrantikM <145676123+PrantikM@users.noreply.github.com>
2025-03-11 19:12:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 from transformers import pipeline
-
-import torch
-import torch.nn.functional as F
-
-classifier = pipeline("sentiment-analysis")
-
-res = classifier(["Congratulations! You have been selected.",
-                  "I hope you dont hate it."])
-for result in res:
-  print(result)
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
